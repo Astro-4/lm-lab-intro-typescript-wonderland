0e553ef1fe3e90c81629c52bafd264e365d72ca4
Tefo-K <tefokeabetswe4@gmail.com>
2023-07-27 17:07:20 +0100

    Fix: added the correct ball and mallet in game.hit

--- src/chapter_4/chapter_4_croquet.ts
+++ src/chapter_4/chapter_4_croquet.ts
@@ -52,8 +52,8 @@ export function playCroquet(): void {
     },
   };
 
-  // 👉 FIXME ❌ These parameters to hit() aren't right!
-  if (game && game.hit && game.hit(2468, 8642)) {
+  // 👉 FIXED
+  if (game && game.hit && game.hit("Hedgehog", "Flamingo")) {
     print("🏆 Success! You scored! 🏆");
     print("Time to chat with the queen...");
     return askQuestion("Press ENTER to continue! ", meetTheQueen);
